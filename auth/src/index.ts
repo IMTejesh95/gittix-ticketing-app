d817145ac76aba28104c646129bbdbde893960e6
Tejesh Chauragade <chauragade.tejesh@gmail.com>
2021-11-13 21:52:41 +0530

    minor fixes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongo from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
@@ -12,9 +13,13 @@ import { NotFoundError } from './errors/not-found-error';
 
 
 const app = express();
-
+app.set('trust proxy', true);
 // middlewares
 app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}));
 
 
 // routes
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -29,6 +30,16 @@ router.post(
 
         const user = User.build({email, password});
         await user.save();
+
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, 'asdf');
+
+        req.session = {
+            jwt: userJwt
+        }
+
         
         res.status(201).json(user);
     });
