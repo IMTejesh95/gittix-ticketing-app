6cc09e180a75ba8c78add7b5f0204ee41ee6e4b0
Tejesh Chauragade <chauragade.tejesh@gmail.com>
2022-02-27 18:39:55 +0530

    feat: add helper function for signin in test env

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,11 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import { app } from "../app";
+import request from "supertest";
+
+declare global {
+  function signin(): Promise<string[]>;
+}
 
 let mongo: MongoMemoryServer;
 beforeAll(async () => {
@@ -22,3 +28,19 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async (): Promise<string[]> => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+};
