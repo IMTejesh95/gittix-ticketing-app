baf13849ff1a11e3423dca350b59c5bd8422cbf3
Tejesh Chauragade <chauragade.tejesh@gmail.com>
2022-05-07 21:46:57 +0530

    feat: add listners for order created & cancelled

---
+++ payments/src/events/listners/order-cancelled-listner.ts
@@ -0,0 +1,29 @@
+import {
+  Listner,
+  NotFoundError,
+  OrderCancelledEvent,
+  OrderStatus,
+  Subjects,
+} from "@tjgittix/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { QUEUE_GROUP_NAME } from "../queue-group-name";
+
+export class OrderCancelledListner extends Listner<OrderCancelledEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName: string = QUEUE_GROUP_NAME;
+
+  async onMessage(
+    data: OrderCancelledEvent["data"],
+    message: Message
+  ): Promise<void> {
+    const order = await Order.findByEventData(data);
+
+    if (!order) throw new NotFoundError();
+
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+
+    message.ack();
+  }
+}
