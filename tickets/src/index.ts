08254d17b1c57c98d98e8233604b52fb16d499fa
Tejesh Chauragade <chauragade.tejesh@gmail.com>
2022-05-20 16:06:03 +0530

    feat: deploy worflow trigger

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCreatedListner } from "./events/listners/order-created-listner";
 import { stan } from "./events/nats-client";
 
 const start = async () => {
-  console.log("@gittix/tickets starting up...");
+  console.log("@gittix/tickets starting up..");
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined!");
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined!");
   if (!process.env.NATS_URL) throw new Error("NATS_URL must be defined!");
